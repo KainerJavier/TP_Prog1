f64060d62c3c12b47a101f986fce7594005d62b4
KainerJavier <kjekainer@gmail.com>
2022-10-20 18:08:15 -0300

    login corregido

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ require_once 'controladorSesion.php';
 if (!isset($_POST['usuario']) || !isset($_POST['clave'])) {
     $redirigir = 'index.php?mensaje=Error: Falta un campo obligatorio';
 } else {
-    $cs = new ControladorSesion();
+    $cs = new controladorSesion();
     $login = $cs->login($_POST['usuario'], $_POST['clave']);
     if ($login[0] === true) {
         $redirigir = 'home.php';
