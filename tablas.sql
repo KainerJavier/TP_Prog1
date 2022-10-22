86d66c3acf399382aa33a4c14947f9d88d3c8f3d
KainerJavier <kjekainer@gmail.com>
2022-10-21 22:14:15 -0300

    tablas

--- tablas.sql
+++ tablas.sql
@@ -1,24 +1,82 @@
-CREATE TABLE usuarios (
-  id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  usuario varchar(45) NOT NULL,
-  clave varchar(255) NOT NULL,
-  nombre varchar(200) NOT NULL,
-  apellido varchar(200) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY usuario (usuario)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: carros
+-- ------------------------------------------------------
+-- Server version	8.0.26
 
-INSERT INTO usuarios (usuario, clave, nombre, apellido)
-VALUES (1,'ejemplo','1234','Javier','Kainer');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE carros (
-  id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  marca varchar(45) NOT NULL,
-  modelo varchar(255) NOT NULL,
-  año varchar(200) NOT NULL,
-  color varchar(200) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY usuario (usuario)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+--
+-- Table structure for table `carros`
+--
 
+DROP TABLE IF EXISTS `carros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `carros` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `marca` varchar(40) NOT NULL,
+  `modelo` varchar(20) NOT NULL,
+  `año` varchar(20) NOT NULL,
+  `color` int NOT NULL,
+  PRIMARY KEY (`id`),
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `autos`
+--
+
+LOCK TABLES `carros` WRITE;
+/*!40000 ALTER TABLE `carros` DISABLE KEYS */;
 INSERT INTO carros VALUES (1,'Toyota','Corolla','2018','Gris'),(2,'Ford','Focus','2014','Negro'),(3,'Fiat','Cronos','2022','Rojo'),(4,'Chevrolet','Cruze','2017','Blanco');
+/*!40000 ALTER TABLE `carros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(45) NOT NULL,
+  `clave` varchar(255) NOT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `apellido` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `usuario` (`usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'ejemplo','1234','Javier','Kainer');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-21 22:10:56
